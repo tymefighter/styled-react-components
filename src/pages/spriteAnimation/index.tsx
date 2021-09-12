8d4172ce149456e5f891f56ec638c11d02ef1998
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-09-12 11:59:43 +0530

    feat: adds simple sprite animator component use case

--- src/pages/spriteAnimation/index.tsx
+++ src/pages/spriteAnimation/index.tsx
@@ -1,8 +1,31 @@
 // Components
 import { PageLayout } from 'modules/pageLayout';
+import { SpriteAnimator } from 'modules/spriteAnimator';
+
+// Styles
+import styles from './SpriteAnimationPage.module.scss';
 
 export const SpriteAnimationPage = () => (
   <PageLayout>
-    
+    <div className={styles.container}>
+      <SpriteAnimator
+        width={60}
+        height={60}
+        imageWidth={480}
+        imageHeight={240}
+        src="/images/sprites.png"
+        frameIndices={[
+          { row: 2, column: 0 },
+          { row: 2, column: 1 },
+          { row: 2, column: 2 },
+          { row: 2, column: 3 },
+          { row: 2, column: 4 },
+          { row: 2, column: 5 },
+          { row: 2, column: 6 },
+          { row: 2, column: 7 },
+        ]}
+        delay={100}
+      />  
+    </div>
   </PageLayout>
 );
